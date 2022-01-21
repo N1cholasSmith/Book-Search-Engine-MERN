dba715cbf8469bf649cea975742b4374000995dc
nicholas <n1ckorish14@gmail.com>
2022-01-21 15:11:16 +1100

    initial commit, downloading apollo and graphql dependencies, set up server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+// adding Apollo to the server
+const { ApolloServer } = require('apollo-server-express')
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
